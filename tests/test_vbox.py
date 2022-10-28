4e29c1252a5c333d99c78b8cae6c23be3858d45e
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-10-28 16:43:37 +0200

    DIS-1430 Add test for VBOX-file parser

---
+++ tests/test_vbox.py
@@ -0,0 +1,22 @@
+from io import StringIO
+
+from dissect.hypervisor.disk.vdi import Vbox
+
+
+def test_vbox():
+
+    xml = """<?xml version="1.0"?>
+                <VirtualBox xmlns="http://www.virtualbox.org/">
+                    <Machine>
+                        <MediaRegistry>
+                            <HardDisks>
+                                <HardDisk location="os2warp4.vdi" format="VDI" type="Normal" />
+                            </HardDisks>
+                        </MediaRegistry>
+                    </Machine>
+                </VirtualBox>
+    """
+
+    with StringIO(xml) as fh:
+        vbox = Vbox(fh)
+        assert next(vbox.disks()) == "os2warp4.vdi"
