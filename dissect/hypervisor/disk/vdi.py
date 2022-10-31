a29d26e165139802f7c006582dcbc577ff2c0e28
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-10-31 16:10:10 +0100

    Denote xml as private property (DIS-1430)

--- dissect/hypervisor/disk/vdi.py
+++ dissect/hypervisor/disk/vdi.py
@@ -71,8 +71,8 @@ class Vbox:
     VBOX_XML_NAMESPACE = "{http://www.virtualbox.org/}"
 
     def __init__(self, fh):
-        self.xml = ElementTree.fromstring(fh.read())
+        self._xml = ElementTree.fromstring(fh.read())
 
     def disks(self):
-        for hdd_elem in self.xml.findall(f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@format='VDI'][@type='Normal']"):
+        for hdd_elem in self._xml.findall(f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@format='VDI'][@type='Normal']"):
             yield hdd_elem.attrib["location"]
