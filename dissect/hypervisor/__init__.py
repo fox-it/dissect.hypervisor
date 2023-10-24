9ebd3eeb5fa67bd4d50f63b614117e0a8a33f2aa
Schamper <1254028+Schamper@users.noreply.github.com>
2023-10-24 20:10:26 +0200

    Remove WIM code in favor of dissect.archive

--- dissect/hypervisor/__init__.py
+++ dissect/hypervisor/__init__.py
@@ -1,4 +1,4 @@
-from dissect.hypervisor.backup import vma, wim, xva
+from dissect.hypervisor.backup import vma, xva
 from dissect.hypervisor.descriptor import hyperv, ovf, pvs, vbox, vmx
 from dissect.hypervisor.disk import hdd, qcow2, vdi, vhd, vhdx, vmdk
 from dissect.hypervisor.util import envelope, vmtar
@@ -18,6 +18,5 @@ __all__ = [
     "vmdk",
     "vmtar",
     "vmx",
-    "wim",
     "xva",
 ]
