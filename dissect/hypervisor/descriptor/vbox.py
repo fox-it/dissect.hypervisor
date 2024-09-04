b9c4860c88f67511894819bcef7253ab9aa4500d
Roel de Jong <roel.dejong@fox-it.com>
2024-09-04 15:36:32 +0200

    Fix detection of vdi harddisks.
    Sometimes the vbox configuration file might specifiy the format attribute of a HardDisk using lower case (vdi).
    This patch ensures that both lower and upper case are supported.

--- dissect/hypervisor/descriptor/vbox.py
+++ dissect/hypervisor/descriptor/vbox.py
@@ -11,7 +11,8 @@ class VBox:
         self._xml: Element = ElementTree.fromstring(fh.read())
 
     def disks(self) -> Iterator[str]:
-        for hdd_elem in self._xml.findall(
-            f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@format='VDI'][@type='Normal']"
-        ):
-            yield hdd_elem.attrib["location"]
+        for hdd_elem in self._xml.findall(f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@type='Normal']"):
+            # Allow format specifier to be case-insensitive (i.e. VDI, vdi)
+            format_attr = hdd_elem.get("format")
+            if format_attr and format_attr.lower() == "vdi":
+                yield hdd_elem.attrib["location"]
--- tests/test_vbox.py
+++ tests/test_vbox.py
@@ -20,3 +20,22 @@ def test_vbox():
     with StringIO(xml.strip()) as fh:
         vbox = VBox(fh)
         assert next(vbox.disks()) == "os2warp4.vdi"
+
+
+def test_vbox_lowercase_disk_format():
+    xml = """
+    <?xml version="1.0"?>
+    <VirtualBox xmlns="http://www.virtualbox.org/">
+        <Machine>
+            <MediaRegistry>
+                <HardDisks>
+                    <HardDisk location="WinDev2407Eval-disk001.vdi" format="vdi" type="Normal" />
+                </HardDisks>
+            </MediaRegistry>
+        </Machine>
+    </VirtualBox>
+    """
+
+    with StringIO(xml.strip()) as fh:
+        vbox = VBox(fh)
+        assert next(vbox.disks()) == "WinDev2407Eval-disk001.vdi"
