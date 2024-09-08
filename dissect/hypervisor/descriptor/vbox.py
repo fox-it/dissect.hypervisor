98a9a660cc886ccf5ecd21f575e47401ed199d26
twiggler <dejongroel@gmail.com>
2024-09-08 16:46:36 +0200

    Fix detection of vdi harddisks inside a vbox file. (#42)

--- dissect/hypervisor/descriptor/vbox.py
+++ dissect/hypervisor/descriptor/vbox.py
@@ -11,7 +11,7 @@ class VBox:
         self._xml: Element = ElementTree.fromstring(fh.read())
 
     def disks(self) -> Iterator[str]:
-        for hdd_elem in self._xml.findall(
-            f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@format='VDI'][@type='Normal']"
-        ):
-            yield hdd_elem.attrib["location"]
+        for hdd_elem in self._xml.findall(f".//{self.VBOX_XML_NAMESPACE}HardDisk[@location][@type='Normal']"):
+            # Allow format specifier to be case-insensitive (i.e. VDI, vdi)
+            if (format := hdd_elem.get("format")) and format.lower() == "vdi":
+                yield hdd_elem.attrib["location"]
