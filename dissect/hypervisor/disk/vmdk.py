bfa420779a95100695bbe0fffce02be5cfeb1c74
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2024-11-26 11:26:12 +0100

    simplify regex

--- dissect/hypervisor/disk/vmdk.py
+++ dissect/hypervisor/disk/vmdk.py
@@ -408,10 +408,10 @@ RE_EXTENT_DESCRIPTOR = re.compile(
     (?P<access_mode>RW|RDONLY|NOACCESS)\s
     (?P<sectors>\d+)\s
     (?P<type>SPARSE|ZERO|FLAT|VMFS|VMFSSPARSE|VMFSRDM|VMFSRAW)
-    (\s?(?P<filename>\".+\")\s?)?
-    (\s?(?P<start_sector>\d+)\s?)?
-    (\s?(?P<partition_uuid>\S+)\s?)?
-    (?P<device_identifier>\S+)?
+    (\s(?P<filename>\".+\"))?
+    (\s(?P<start_sector>\d+))?
+    (\s(?P<partition_uuid>\S+))?
+    (\s(?P<device_identifier>\S+))?
     $
     """,
     re.VERBOSE,
