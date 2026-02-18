ae08df8523e5a07a1b2693d422aa405d2dfa9419
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2026-02-18 16:53:37 +0100

    Improve VDI (#72)

--- dissect/hypervisor/disk/c_vdi.py
+++ dissect/hypervisor/disk/c_vdi.py
@@ -2,90 +2,97 @@ from __future__ import annotations
 
 from dissect.cstruct import cstruct
 
-# https://www.virtualbox.org/browser/vbox/trunk/src/VBox/Storage/VDICore.h
-# https://forums.virtualbox.org/viewtopic.php?t=8046
-# 0000      3C 3C 3C 20 53 75 6E 20 78 56 4D 20 56 69 72 74    <<< Sun xVM Virt
-# 0010      75 61 6C 42 6F 78 20 44 69 73 6B 20 49 6D 61 67    ualBox Disk Imag
-# 0020      65 20 3E 3E 3E 0A 00 00 00 00 00 00 00 00 00 00    e >>>
-# 0030      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-#
-# 0040      7F 10 DA BE                                        Image Signature
-#                       01 00 01 00                            Version 1.1
-#                                   90 01 00 00                Size of Header(0x190)
-#                                               01 00 00 00    Image Type (Dynamic VDI)
-# 0050      00 00 00 00                                        Image Flags
-#                       00 00 00 00 00 00 00 00 00 00 00 00    Image Description
-# 0060-001F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-# 0150      00 00 00 00
-#                       00 02 00 00                            offsetBlocks
-#                                   00 20 00 00                offsetData
-#                                               00 00 00 00    #Cylinders (0)
-# 0160      00 00 00 00                                        #Heads (0)
-#                       00 00 00 00                            #Sectors (0)
-#                                   00 02 00 00                SectorSize (512)
-#                                               00 00 00 00     -- unused --
-# 0170      00 00 00 78 00 00 00 00                            DiskSize (Bytes)
-#                                   00 00 10 00                BlockSize
-#                                               00 00 00 00    Block Extra Data (0)
-# 0180      80 07 00 00                                        #BlocksInHDD
-#                       0B 02 00 00                            #BlocksAllocated
-#                                   5A 08 62 27 A8 B6 69 44    UUID of this VDI
-# 0190      A1 57 E2 B2 43 A5 8F CB
-#                                   0C 5C B1 E3 C5 73 ED 40    UUID of last SNAP
-# 01A0      AE F7 06 D6 20 69 0C 96
-#                                   00 00 00 00 00 00 00 00    UUID link
-# 01B0      00 00 00 00 00 00 00 00
-#                                   00 00 00 00 00 00 00 00    UUID Parent
-# 01C0      00 00 00 00 00 00 00 00
-#                                   CF 03 00 00 00 00 00 00    -- garbage / unused --
-# 01D0      3F 00 00 00 00 02 00 00 00 00 00 00 00 00 00 00    -- garbage / unused --
-# 01E0      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    -- unused --
-# 01F0      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    -- unused --
-
+# https://github.com/VirtualBox/virtualbox/blob/main/src/VBox/Storage/VDICore.h
 vdi_def = """
-enum ImageType : uint32 {
-    Dynamic         = 0x01,
-    Fixed           = 0x02,
-    Undo            = 0x03,
-    Differencing    = 0x04
+enum VDI_IMAGE_TYPE {
+    /** Normal dynamically growing base image file. */
+    NORMAL          = 1,
+    /** Preallocated base image file of a fixed size. */
+    FIXED,
+    /** Dynamically growing image file for undo/commit changes support. */
+    UNDO,
+    /** Dynamically growing image file for differencing support. */
+    DIFF,
 };
 
-flag ImageFlags : uint32 {
-    None            = 0x00000000,
-    Split2G         = 0x00000001,
-    ZeroExpand      = 0x00000002
+flag VDI_IMAGE_FLAGS {
+    /** Fill new blocks with zeroes while expanding image file. Only valid
+     * for newly created images, never set for opened existing images. */
+    ZERO_EXPAND     = 0x0100,
 };
 
-struct HeaderDescriptor {
-    char        FileInfo[64];
-    uint32      Signature;
-    uint32      Version;
-    uint32      HeaderSize;
-    ImageType   ImageType;
-    ImageFlags  ImageFlags;
-    char        ImageDescription[256];
-    uint32      BlocksOffset;
-    uint32      DataOffset;
-    uint32      NumCylinders;
-    uint32      NumHeads;
-    uint32      NumSectors;
-    uint32      SectorSize;
-    uint32      Unused1;
-    uint64      DiskSize;
-    uint32      BlockSize;
-    uint32      BlockExtraData;
-    uint32      BlocksInHDD;
-    uint32      BlocksAllocated;
-    char        UUIDVDI[16];
-    char        UUIDSNAP[16];
-    char        UUIDLink[16];
-    char        UUIDParent[16];
-};
+typedef struct VDIDISKGEOMETRY {
+    /** Cylinders. */
+    uint32_t        Cylinders;
+    /** Heads. */
+    uint32_t        Heads;
+    /** Sectors per track. */
+    uint32_t        Sectors;
+    /** Sector size. (bytes per sector) */
+    uint32_t        Sector;
+} VDIDISKGEOMETRY, *PVDIDISKGEOMETRY;
+
+typedef struct VDIPREHEADER {
+    /** Just text info about image type, for eyes only. */
+    char            szFileInfo[64];
+    /** The image signature (VDI_IMAGE_SIGNATURE). */
+    uint32_t        u32Signature;
+    /** The image version (VDI_IMAGE_VERSION). */
+    uint32_t        u32Version;
+} VDIPREHEADER, *PVDIPREHEADER;
+
+/**
+ * Size of Comment field of HDD image header.
+ */
+#define VDI_IMAGE_COMMENT_SIZE      256
+
+/* NOTE: All the header versions are additive, so just use the latest one. */
+typedef struct VDIHEADER1PLUS {
+    /** Size of this structure in bytes. */
+    uint32_t        cbHeader;
+    /** The image type (VDI_IMAGE_TYPE_*). */
+    VDI_IMAGE_TYPE  u32Type;
+    /** Image flags (VDI_IMAGE_FLAGS_*). */
+    VDI_IMAGE_FLAGS fFlags;
+    /** Image comment. (UTF-8) */
+    char            szComment[VDI_IMAGE_COMMENT_SIZE];
+    /** Offset of blocks array from the beginning of image file.
+     * Should be sector-aligned for HDD access optimization. */
+    uint32_t        offBlocks;
+    /** Offset of image data from the beginning of image file.
+     * Should be sector-aligned for HDD access optimization. */
+    uint32_t        offData;
+    /** Legacy image geometry (previous code stored PCHS there). */
+    VDIDISKGEOMETRY LegacyGeometry;
+    /** Was BIOS HDD translation mode, now unused. */
+    uint32_t        u32Dummy;
+    /** Size of disk (in bytes). */
+    uint64_t        cbDisk;
+    /** Block size. (For instance VDI_IMAGE_BLOCK_SIZE.) Should be a power of 2! */
+    uint32_t        cbBlock;
+    /** Size of additional service information of every data block.
+     * Prepended before block data. May be 0.
+     * Should be a power of 2 and sector-aligned for optimization reasons. */
+    uint32_t        cbBlockExtra;
+    /** Number of blocks. */
+    uint32_t        cBlocks;
+    /** Number of allocated blocks. */
+    uint32_t        cBlocksAllocated;
+    /** UUID of image. */
+    char            uuidCreate[16];
+    /** UUID of image's last modification. */
+    char            uuidModify[16];
+    /** Only for secondary images - UUID of previous image. */
+    char            uuidLinkage[16];
+    /** Only for secondary images - UUID of previous image's last modification. */
+    char            uuidParentModify[16];
+    /** LCHS image geometry (new field in VDI1.2 version. */
+    VDIDISKGEOMETRY Geometry;
+} VDIHEADER1PLUS, *PVDIHEADER1PLUS;
 """
 
 c_vdi = cstruct().load(vdi_def)
 
-VDI_SIGNATURE = 0xBEDA107F
-
-UNALLOCATED = -1
-SPARSE = -2
+VDI_IMAGE_SIGNATURE = 0xBEDA107F
+VDI_IMAGE_BLOCK_FREE = ~0
+VDI_IMAGE_BLOCK_ZERO = ~1
--- dissect/hypervisor/disk/vdi.py
+++ dissect/hypervisor/disk/vdi.py
@@ -1,62 +1,140 @@
 from __future__ import annotations
 
-import array
-from typing import BinaryIO
+from pathlib import Path
+from typing import TYPE_CHECKING, BinaryIO
 
 from dissect.util.stream import AlignedStream
+from dissect.util.xmemoryview import xmemoryview
 
-from dissect.hypervisor.disk.c_vdi import SPARSE, UNALLOCATED, VDI_SIGNATURE, c_vdi
+from dissect.hypervisor.disk.c_vdi import VDI_IMAGE_BLOCK_FREE, VDI_IMAGE_BLOCK_ZERO, VDI_IMAGE_SIGNATURE, c_vdi
 from dissect.hypervisor.exceptions import Error
 
+if TYPE_CHECKING:
+    from types import TracebackType
 
-class VDI(AlignedStream):
-    def __init__(self, fh: BinaryIO, parent: VDI | None = None):
-        self.fh = fh
+    from typing_extensions import Self
+
+
+class VDI:
+    """VirtualBox Virtual Disk Image (VDI) implementation.
+
+    Use :method:`open` to get a stream for reading from the VDI file. The stream will handle reading
+    from the parent disk if necessary (and provided).
+
+    If provided with a file-like object, the caller is responsible for closing it.
+    When provided with a path, the VDI class will manage the file handle.
+
+    If providing a parent file-like object, the caller is responsible for the lifecycle of that object.
+
+    Args:
+        fh: File-like object or path of the VDI file.
+        parent: Optional file-like object for the parent disk (for differencing disks).
+    """
+
+    def __init__(self, fh: BinaryIO | Path, parent: BinaryIO | None = None):
+        if isinstance(fh, Path):
+            self.path = fh
+            self.fh = self.path.open("rb")
+        else:
+            self.path = None
+            self.fh = fh
+
         self.parent = parent
-        self.header = c_vdi.HeaderDescriptor(fh)
 
-        if self.header.Signature != VDI_SIGNATURE:
-            raise Error("Not a VDI header")
+        self.fh.seek(0)
+        self.preheader = c_vdi.VDIPREHEADER(self.fh)
+        if self.preheader.u32Signature != VDI_IMAGE_SIGNATURE:
+            raise Error(
+                f"Invalid VDI signature, expected {VDI_IMAGE_SIGNATURE:#08X}, got {self.preheader.u32Signature:#08X}"
+            )
 
-        fh.seek(-1, 2)
-        self.file_size = fh.tell()
+        self.header = c_vdi.VDIHEADER1PLUS(self.fh)
 
-        self.fh.seek(self.header.BlocksOffset)
+    def __enter__(self) -> Self:
+        return self
 
-        mapbuf = self.fh.read(4 * self.header.BlocksInHDD)
-        self.map = array.array("i")
-        try:
-            self.map.frombytes(mapbuf)
-        except AttributeError:
-            self.map.fromstring(mapbuf)
+    def __exit__(
+        self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None
+    ) -> None:
+        self.close()
 
-        self.data_offset = self.header.DataOffset
-        self.block_size = self.header.BlockSize
-        self.sector_size = self.header.SectorSize
-        super().__init__(size=self.header.DiskSize)
+    @property
+    def type(self) -> c_vdi.VDI_IMAGE_TYPE:
+        """The type of the VDI file."""
+        return self.header.u32Type
+
+    @property
+    def flags(self) -> c_vdi.VDI_IMAGE_FLAGS:
+        """The flags of the VDI file."""
+        return self.header.fFlags
+
+    @property
+    def size(self) -> int:
+        """The size of the virtual disk."""
+        return self.header.cbDisk
+
+    @property
+    def block_size(self) -> int:
+        """The size of each block in the VDI file."""
+        return self.header.cbBlock
+
+    @property
+    def data_offset(self) -> int:
+        """The offset to the data blocks."""
+        return self.header.offData
+
+    @property
+    def blocks_offset(self) -> int:
+        """The offset to the block allocation table."""
+        return self.header.offBlocks
+
+    @property
+    def number_of_blocks(self) -> int:
+        """The number of blocks in the VDI file."""
+        return self.header.cBlocks
+
+    def open(self) -> VDIStream:
+        """Open a stream to read from the VDI file."""
+        return VDIStream(self)
+
+    def close(self) -> None:
+        """Close the VDI file handle."""
+        if self.path is not None:
+            self.fh.close()
+
+
+class VDIStream(AlignedStream):
+    def __init__(self, vdi: VDI):
+        self.vdi = vdi
+        self.block_size = vdi.block_size
+
+        self.fh = self.vdi.fh
+        self.fh.seek(self.vdi.blocks_offset)
+        self.map = xmemoryview(self.fh.read(4 * self.vdi.number_of_blocks), "<i")
+        super().__init__(vdi.size, align=self.block_size)
 
     def _read(self, offset: int, length: int) -> bytes:
-        block_idx, block_offset = divmod(offset, self.block_size)
+        result = []
 
-        bytes_read = []
+        block_idx, offset_in_block = divmod(offset, self.block_size)
         while length > 0:
-            read_len = min(length, max(length, self.block_size))
+            read_len = min(length, max(length, self.block_size - offset_in_block))
 
             block = self.map[block_idx]
-
-            if block == UNALLOCATED:
-                if self.parent:
-                    bytes_read.append(self.parent._read(offset, read_len))
+            if block == VDI_IMAGE_BLOCK_FREE:
+                if self.vdi.parent is not None:
+                    self.vdi.parent.seek(offset)
+                    result.append(self.vdi.parent.read(read_len))
                 else:
-                    bytes_read.append(b"\x00" * read_len)
-            elif block == SPARSE:
-                bytes_read.append(b"\x00" * read_len)
+                    result.append(b"\x00" * read_len)
+            elif block == VDI_IMAGE_BLOCK_ZERO:
+                result.append(b"\x00" * read_len)
             else:
-                self.fh.seek(self.data_offset + (block * self.block_size) + block_offset)
-                bytes_read.append(self.fh.read(read_len))
+                self.fh.seek(self.vdi.data_offset + (block * self.block_size) + offset_in_block)
+                result.append(self.fh.read(read_len))
 
             offset += read_len
             length -= read_len
             block_idx += 1
 
-        return b"".join(bytes_read)
+        return b"".join(result)
